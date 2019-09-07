baf8c1f31a90e65844dfc8c0ca3038e57f5c6fc6
Christopher Rice <Aioxas@users.noreply.github.com>
2019-09-06 20:07:39 -0400

    V3 horoscope regex fix (#40)
    * Change regex to match with or without class
    
    * I give up, one by one.
    
    From aiks
    
    * I give up, one by one
    
    from aiks

--- horoscope/__init__.py
+++ horoscope/__init__.py
@@ -1,8 +1,5 @@
-from redbot.core import commands, data_manager
 from .horoscope import Horoscope
 
 
-def setup(bot: commands.Bot):
-    n = Horoscope(bot)
-    data_manager.load_bundled_data(n, __file__)
-    bot.add_cog(n)
+def setup(bot):
+    bot.add_cog(Horoscope(bot))
