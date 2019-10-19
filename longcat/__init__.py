a91d4df1b4b4c12bebb291f8a47e3a5ee543d755
aikaterna <20862007+aikaterna@users.noreply.github.com>
2019-10-18 21:20:11 -0700

    [Longcat] load_bundled_data is loooooong gone (#43)
    Gets rid of an informational message on cog load about the depreciation.

--- longcat/__init__.py
+++ longcat/__init__.py
@@ -1,8 +1,7 @@
-from redbot.core import data_manager, commands
+from redbot.core import commands
 from .longcat import Longcat
 
 
 def setup(bot: commands.Bot):
     cog = Longcat(bot)
-    data_manager.load_bundled_data(cog, __file__)
     bot.add_cog(cog)
